8106ffe406ef5300206c29d7a5d14395654c136c
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-27 02:21:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  the_bro_philosophers = ["Descartes", "Kant", "Schleiermacher", ]
+  the_bro_philosophers = ["Descartes", "Kant", "Schleiermacher", "Kierkegaard"]
   
 end
 
